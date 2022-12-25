6d60d64633a2f1d334c4ab7d5b84cfdee86b90bb
Arif Burak Demiray <burakdemiray09@hotmail.com>
2022-12-25 15:22:45 +0300

    fix: jpa transaction manager to manage transactions

--- src/main/java/com/bookretail/config/TransactionConfig.java
+++ src/main/java/com/bookretail/config/TransactionConfig.java
@@ -1,25 +1,19 @@
 package com.bookretail.config;
 
-import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
 public class TransactionConfig {
 
     @Bean
-    public DataSource dataSource() {
-        return new MariaDbDataSource();
-    }
-
-    @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(dataSource());
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
     }
 }
